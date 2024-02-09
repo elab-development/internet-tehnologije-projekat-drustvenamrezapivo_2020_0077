afdd078139b6168be43d40373a773eb6508a0ff9
perchedinthedark <divacranko@gmail.com>
2024-02-09 16:00:48 +0100

    Promenjen postspage, poboljsanje filtriranje i paginacija, izmenjeni useeffectovi

--- domacireact/src/components/PostsPage.jsx
+++ domacireact/src/components/PostsPage.jsx
@@ -21,10 +21,11 @@ function PostsPage({renderAll,setRenderAll}) {
   const [azurirajPosts, setAzurirajPosts] = useState(false);
 
 
-
-  const[currentPosts,setCurrentPosts]=useState([]);
-  const [allPosts, setAllPosts] = useState([]); 
-  const [filteredPosts, setFilteredPosts] = useState([]); 
+  const[pomocnaProm1,setPomocnaProm1]=useState(true);
+  const[pomocnaProm2,setPomocnaProm2]=useState(true);
+  const [maxPages,setMaxPages]=useState(2);
+  const [posts,setPosts]=useState([]);
+  
 
 
   
@@ -32,64 +33,157 @@ function PostsPage({renderAll,setRenderAll}) {
   
    
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(5);
- 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
+  const[najpomocnija,setNajpomocnija]=useState(1);
+
   
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
-    const indexOfLastPost = pageNumber * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    setCurrentPosts(filteredPosts.slice(indexOfFirstPost, indexOfLastPost));
-  };
+  const changePage = (page) => {//dodato
+    setCurrentPage(page);
+   
+    setPomocnaProm2(pomocnaProm2=>!pomocnaProm2);
+};
   
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
   };
+
+  useEffect(() => {
+    
+      setCurrentPage(1);
+      setMaxPages(2);
+}, []);
   
   
   useEffect(() => {
     console.log("use effect postsPage");
-    
+   
     let putanja = '';
     console.log(params);
     console.log(location);
     if (location.pathname.startsWith('/posts/')) {
-      putanja = 'api/postsOfFriends/';
+      putanja = 'api/postsOfFriends/'+params.user_id+"?filter="+filter+"&firstN="+currentPage;
     }
     if (location.pathname.startsWith('/explore/')) {
-      putanja = 'api/postsOfEnemies/';
+      putanja = 'api/postsOfEnemies/'+params.user_id+"?filter="+filter+"&firstN="+currentPage;
     }
     if (location.pathname.startsWith('/profile/')) {
-      putanja = 'api/postsOfProfile/';
+      putanja = 'api/postsOfProfile/'+params.user_id+"?filter="+filter+"&firstN="+currentPage;
     }
-  
+    if(location.pathname.startsWith('/trial')){
+      putanja='api/posts';
+    }
+ 
     axios
-      .get(putanja + params.user_id, {
+      .get(putanja, {
         headers: {
           'Authorization': `Bearer ${window.sessionStorage.auth_token}`,
         },
       })
       .then((response) => {
-        setAllPosts(response.data.posts);
-        setFilteredPosts(response.data.posts); 
-        setCurrentPosts(response.data.posts.slice(indexOfFirstPost, indexOfLastPost));
+        console.log(response);
+        setPosts(response.data.posts);
+       
       })
       .catch((error) => {
+       
         console.log(error);
       });
   }, [azurirajPosts,params]);
+ 
+ 
 
+ 
   useEffect(() => {
-    
-    
-    const filtered = allPosts.filter((post) => post.location.includes(filter) || post.content.includes(filter));
-    setFilteredPosts(filtered);
-    
-    setCurrentPage(1);
-    setCurrentPosts(filtered.slice(0, postsPerPage)); 
-  }, [filter, allPosts]);
+    console.log("use effect postsPage");
+   
+    let putanja = '';
+    console.log(params);
+    console.log(location);
+    if (location.pathname.startsWith('/posts/')) {
+      putanja = 'api/postsOfFriends/'+params.user_id+"?filter="+filter+"&firstN="+currentPage;
+    }
+    if (location.pathname.startsWith('/explore/')) {
+      putanja = 'api/postsOfEnemies/'+params.user_id+"?filter="+filter+"&firstN="+currentPage;
+    }
+    if (location.pathname.startsWith('/profile/')) {
+      putanja = 'api/postsOfProfile/'+params.user_id+"?filter="+filter+"&firstN="+currentPage;
+    }
+    if(location.pathname.startsWith('/trial')){
+      putanja='api/posts';
+    }
+ 
+    axios
+      .get(putanja, {
+        headers: {
+          'Authorization': `Bearer ${window.sessionStorage.auth_token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        setPosts(response.data.posts);
+        setCurrentPage(1);
+        setMaxPages(2);
+        // setFilteredPosts(response.data.posts);
+        // setCurrentPosts(response.data.posts.slice(indexOfFirstPost, indexOfLastPost));
+      })
+      .catch((error) => {
+       
+        console.log(error);
+      });
+  }, [pomocnaProm1]);
+ 
+ 
+  useEffect(() => {
+    console.log("use effect postsPage");
+   
+    let putanja = '';
+    console.log(params);
+    console.log(location);
+    if (location.pathname.startsWith('/posts/')) {
+      putanja = 'api/postsOfFriends/'+params.user_id+"?filter="+filter+"&firstN="+currentPage;
+    }
+    if (location.pathname.startsWith('/explore/')) {
+      putanja = 'api/postsOfEnemies/'+params.user_id+"?filter="+filter+"&firstN="+currentPage;
+    }
+    if (location.pathname.startsWith('/profile/')) {
+      putanja = 'api/postsOfProfile/'+params.user_id+"?filter="+filter+"&firstN="+currentPage;
+    }
+    if(location.pathname.startsWith('/trial')){
+      putanja='api/posts';
+    }
+ 
+    axios
+      .get(putanja, {
+        headers: {
+          'Authorization': `Bearer ${window.sessionStorage.auth_token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        setPosts(response.data.posts);
+        // setCurrentPage(najpomocnija);
+         if(response.data.posts.length==0){
+          // setMaxPages(maxPages=>maxPages-1);
+         }else{
+          // setCurrentPage(currentPage=>currentPage+1);
+          if(currentPage>=maxPages){
+            setMaxPages(maxPages=>maxPages+1);
+          }
+         }
+        //  setCurrentPage(page);
+    // paginate(page);
+   
+ 
+ 
+ 
+        // setFilteredPosts(response.data.posts);
+        // setCurrentPosts(response.data.posts.slice(indexOfFirstPost, indexOfLastPost));
+      })
+      .catch((error) => {
+       
+        console.log(error);
+      });
+  }, [pomocnaProm2]);
 
 
 
@@ -117,26 +211,53 @@ function PostsPage({renderAll,setRenderAll}) {
     alignItems: 'center',
   };
 
+  const paginationStyles = {
+   
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '5px', /* Dodajte prazan prostor između elemenata paginacije */
+
+
+};
+const listItemStyles= {
+flex: '0 0 calc(10% - 5px)', /* Svaki element paginacije zauzima 10% širine reda i ima prazan prostor od 5px */
+listStyle: 'none',
+}
+const buttonStyle={
+
+width: '100%', /* Postavite dugmad paginacije da zauzimaju celu širinu roditeljskog elementa */
+
+}
+
+function getFilteredPosts(){
+  setCurrentPage(currentPage=>1);
+  setMaxPages(maxPages=>2);
+  setPomocnaProm1(pomocnaProm=>!pomocnaProm);
+}
+
   return (
 
     <div className="container" style={{ textAlign: 'center' }}>
-     <Form>
-      <Form.Group controlId="filter">
-        <Form.Label>Filter:</Form.Label>
+        {window.sessionStorage.user_id ? <>
+          <Form>
+      <Form.Group  style={{ marginTop: '20px' }} controlId="filter">
+        {/* <h5>Filter:</h5> */}
         <Form.Control
           type="text"
-          placeholder="Unesite filter"
+          placeholder="Filter posts with location"
           value={filter}
           onChange={handleFilterChange}
           onKeyPress={(e) => {
             if (e.key === 'Enter') {
-              e.preventDefault(); 
+              e.preventDefault();
               handleFilterChange(e);
             }
           }}
         />
+        <Button variant="danger" onClick={(e)=>getFilteredPosts()}>Filter posts</Button>
       </Form.Group>
     </Form>
+    </> : <></>}
 
     <p>Trenutna širina prozora: {currentWindowWidth}px</p>
    
@@ -144,8 +265,8 @@ function PostsPage({renderAll,setRenderAll}) {
     <h1>{location.pathname.startsWith('/profile') ? 'Posts off profile' : ''}</h1>
     <h1>{location.pathname.startsWith('/posts') ? 'Posts of friends' : ''}</h1>
     <div style={postContainerStyle} >
-      {currentPosts ? (
-        currentPosts.map((post) => (
+      {posts ? (
+        posts.map((post) => (
           <div key={`${post.user_id}_${post.post_id}`} style={postStyle}>
             <Post
               renderAll={renderAll}
@@ -163,15 +284,17 @@ function PostsPage({renderAll,setRenderAll}) {
       )}
     </div>
     <div>
-      {filteredPosts.length > postsPerPage && (
-        <ul className="pagination">
-          {Array.from({ length: Math.ceil(filteredPosts.length / postsPerPage) }).map((_, index) => (
-            <li key={index} >
-              <Button className={index + 1 == currentPage ? 'active' : ''}  onClick={() => paginate(index + 1)}>{index + 1}</Button>
-            </li>
-          ))}
+    <div style={{ position: 'fixed', bottom: '20px', left: '20px', zIndex: '999' }}>
+      <ul  style={paginationStyles}>
+            {Array.from({ length: posts.length>=1 ? maxPages : maxPages }).map((_, index) => (
+                <li style={listItemStyles} key={index}>
+                    <Button style={buttonStyle} className={index + 1 === currentPage ? 'active' : ''} onClick={() => changePage(index + 1)}>
+                        {index + 1}
+                    </Button>
+                </li>
+            ))}
         </ul>
-      )}
+        </div>
     </div>
     
   </div>
